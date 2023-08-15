b92a1d0d9ac16c7a6388929394de3e035bc4e53b
naresh2810 <nareshredyy2810@gmail.com>
2023-08-15 16:19:21 +0530

    i have to change the some class

--- src/main/java/com/example/demo/SpringbootRestFullExample01Application.java
+++ src/main/java/com/example/demo/SpringbootRestFullExample01Application.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootRestFullExample01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestFullExample01Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringbootRestFullExample01Application.class, args);
+	System.out.println(run);
+	    
+	
 	}
 
 }
